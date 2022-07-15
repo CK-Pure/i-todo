c113cd580e616688f94e829b72dacbc682425bbf
CK-Pure <akashi_hsw@163.com>
2022-07-15 15:44:03 +0800

    no message

--- i-todo-web/src/main.js
+++ i-todo-web/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/theme-chalk/index.css'
 
+import router from './router/index'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
 
